1c773bfbe3722d5a3205c1a16c420c66f4df1437
luckykumarirai <luckykumari112000@gmail.com>
2022-07-24 15:14:51 +0530

    update changes

--- index.js
+++ index.js
@@ -34,6 +34,8 @@ app.get("/",(req, res)=>{
     res.send("Welcome to Server");
 });
 
-app.listen(5000, ()=>{
-    console.log("server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, ()=>{
+    console.log("server running on port");
 });
